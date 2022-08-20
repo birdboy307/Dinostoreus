10bc0af7da9b29069df8afde62aa1ae802cd6ec9
birdboy307 <birdboy307@gmail.com>
2022-08-20 15:52:45 +1000

    initial

--- pages/cart.js
+++ pages/cart.js
@@ -1,62 +1,16 @@
-/* This example requires Tailwind CSS v2.0+ */
-import { Fragment, useState } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
-import { useCart } from 'react-use-Cart'
+import { useCart } from "react-use-cart"
 
-export default function Example() {
-  const [open, setOpen] = useState(true)
-  const { items, removeItem, updateItemQuantity, cartTotal, emptyCart} = useCart()
+export default function carttest() {
+    const { items, isEmpty, removeItem, updateItemQuantity, totalUniqueItems, emptyCart } = useCart()
+
+    if (isEmpty) return <p>Your cart is empty</p>;
 
   return (
-    <Transition.Root show={open} as={Fragment}>
-      <Dialog as="div" className="relative z-50" onClose={setOpen}>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-in-out duration-500"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in-out duration-500"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-        </Transition.Child>
+    <>
+      <h1>Cart ({totalUniqueItems})</h1>
 
-        <div className="fixed inset-0 overflow-hidden">
-          <div className="absolute inset-0 overflow-hidden">
-            <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
-              <Transition.Child
-                as={Fragment}
-                enter="transform transition ease-in-out duration-500 sm:duration-700"
-                enterFrom="translate-x-full"
-                enterTo="translate-x-0"
-                leave="transform transition ease-in-out duration-500 sm:duration-700"
-                leaveFrom="translate-x-0"
-                leaveTo="translate-x-full"
-              >
-                <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
-                  <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
-                    <div className="flex-1 overflow-y-auto py-6 px-4 sm:px-6">
-                      <div className="flex items-start justify-between">
-                        <Dialog.Title className="text-lg font-medium text-gray-900"> Shopping cart </Dialog.Title>
-                        <div className="ml-3 flex h-7 items-center">
-                          <button
-                            type="button"
-                            className="-m-2 p-2 text-gray-400 transition ease-in-out duration-300 hover:text-gray-500"
-                            onClick={() => setOpen(false)}
-                          >
-                            <span className="sr-only">Close panel</span>
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                                </svg>
-                          </button>
-                        </div>
-                      </div>
-
-                      <div className="mt-8">
-                        <div className="flow-root">
-                          <ul role="list" className="-my-6 divide-y divide-gray-200 list-none">
-                            {items.map((item) => (
+      <ul>
+      {items.map((item) => (
                               <li key={item.id} className="flex py-6">
                                 
                                 <div className="ml-4 flex flex-1 flex-col">
@@ -84,50 +38,8 @@ export default function Example() {
                                 </div>
                               </li>
                             ))}
-                          </ul>
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className="border-t border-gray-200 py-6 px-4 sm:px-6">
-                      <div className="flex justify-between text-base font-medium text-gray-900">
-                        <p>Subtotal</p>
-                        <p>{cartTotal}</p>
-                      </div>
-                      <p className="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
-                      <div className="mt-6 flex">
-                        <a
-                          href="#"
-                          className="w-full flex items-center justify-center rounded-xl border border-transparent bg-blue-500 px-6 py-3 text-base font-medium text-white shadow-sm transition ease-in-out duration-300 hover:bg-blue-700 mx-2"
-                        >
-                          Checkout
-                        </a>
-                        <button onClick={emptyCart} className="py-2 bg-red-500 rounded-xl px-3 transition ease-in-out duration-300 hover:bg-red-700">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                            </svg>
-                        </button>
-                      </div>
-                      <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
-                        <p>
-                          or{' '}
-                          <button
-                            type="button"
-                            className="font-medium text-indigo-600 transition ease-in-out duration-300 hover:text-indigo-500"
-                            onClick={() => setOpen(false)}
-                          >
-                            Continue Shopping<span aria-hidden="true"> &rarr;</span>
-                          </button>
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </div>
-        </div>
-      </Dialog>
-    </Transition.Root>
+      </ul>
+      <button onClick={emptyCart}>clear</button>
+    </>
   )
 }
